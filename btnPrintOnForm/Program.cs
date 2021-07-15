4f7eaa86b118dad03976051e600c62d866fbabfa
Alberto Crivellari <crivealby@gmail.com>
2021-07-15 20:46:32 +0200

    Developing funct printAttrezzatura #time 0,5h
    Create another function for printing the form,
    Scheda Attrezzatura.
    So that the code isnt too concentrated on
    the function ItemEvents, in the if condition of the button.

--- btnPrintOnForm/Program.cs
+++ btnPrintOnForm/Program.cs
@@ -35,9 +35,9 @@ namespace btnPrintOnForm
             }
         }
 
-        public static SAPbouiCOM.Form oForm;
-        public static SAPbouiCOM.Item oItem;
-        public static SAPbouiCOM.Item oOldItem;
+        private static SAPbouiCOM.Form oForm;
+        private static SAPbouiCOM.Item oItem;
+        private static SAPbouiCOM.Item oOldItem;
 
         static void SBO_Application_ItemEvent(string FormUID, ref SAPbouiCOM.ItemEvent pVal, out bool BubbleEvent)
         {
@@ -57,16 +57,22 @@ namespace btnPrintOnForm
                     obt.Caption = "Stampa";
                 }
                 if (pVal.ItemUID == "btnPrint" && (pVal.EventType == SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED || pVal.EventType == SAPbouiCOM.BoEventTypes.et_CLICK) && pVal.BeforeAction == true) {
-                    SAPbouiCOM.Items oItems = oForm.Items;
-                    SAPbouiCOM.EditText valoreCodiceArticolo;
-                    SAPbouiCOM.Item[] prova;
 
+                    printAttrezzatura();
 
-                    valoreCodiceArticolo = (SAPbouiCOM.EditText)oItems.Item("45").Specific;
-                    Application.SBO_Application.MessageBox(valoreCodiceArticolo.Value, 1, "ok", "", "");
                 }
             }
         
+        }
+
+        static void printAttrezzatura()
+        {
+            SAPbouiCOM.Items oItems = oForm.Items;
+            SAPbouiCOM.EditText valoreCodiceArticolo;
+            SAPbouiCOM.Item[] prova;
+
+            valoreCodiceArticolo = (SAPbouiCOM.EditText)oItems.Item("45").Specific;
+            Application.SBO_Application.MessageBox(valoreCodiceArticolo.Value, 1, "ok", "", "");
         }
 
         static void SBO_Application_AppEvent(SAPbouiCOM.BoAppEventTypes EventType)
