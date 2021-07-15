a15440e9c898beace5cf5f759734cfc1506562fa
Alberto Crivellari <crivealby@gmail.com>
2021-07-15 23:53:32 +0200

    Developed class formAttrezzatura #time 0,5h
    Developed the class formAttrezzatura, wrapping a Dictionary,
    of  strings.
    Addeed method to insert, access and construct the class.

--- btnPrintOnForm/formAttrezzatura/formAttrezzatura.cs
+++ btnPrintOnForm/formAttrezzatura/formAttrezzatura.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FirstButtonOnForm.formAttrezzatura
+namespace btnPrintOnForm.formAttrezzatura
 {
     class formAttrezzatura
     {
+        private Dictionary<String, String> form;
+
+        public void insert(string key, string value)
+        {
+            form[key] = value;
+        }
+
+        public bool isEmpty(string key)
+        {
+            return form[key] == null;
+        }
+        public string at(string key){
+            return form[key];
+        }
+
+        public formAttrezzatura()
+        {
+            form = null;
+        }
+
     }
 }
