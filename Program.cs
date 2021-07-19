ac08af61a508b68d9024ee33422c91171d278389
Alberto Crivellari <Alberto Crivellari@Sinapsinet.local>
2021-07-19 17:29:06 +0200

    REFACTOR #time 2min modified program.cs
    Modified Program.cs, copying from buttonClick.
    Removed all events, and letting it have only main.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
-using SAPbouiCOM.Framework;
+using System.Threading.Tasks;
+using System.Linq;
+using SAPbouiCOM;
 
 namespace btnPrintOnForm
 {
@@ -9,86 +12,19 @@ namespace btnPrintOnForm
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+
+        //public SAPbouiCOM.Application oApp;
+
+
         [STAThread]
         static void Main(string[] args)
         {
-            try
-            {
-                Application oApp = null;
-                if (args.Length < 1)
-                {
-                    oApp = new Application();
-                }
-                else
-                {
-                    oApp = new Application(args[0]);
-                }
-              
-                
-                Application.SBO_Application.AppEvent += new SAPbouiCOM._IApplicationEvents_AppEventEventHandler(SBO_Application_AppEvent);
-                Application.SBO_Application.ItemEvent += SBO_Application_ItemEvent;
-                oApp.Run();
-            }
-            catch (Exception ex)
-            {
-                System.Windows.Forms.MessageBox.Show(ex.Message);
-            }
-        }
-
-        private static SAPbouiCOM.Form oForm;
-        private static SAPbouiCOM.Item oItem;
-        private static SAPbouiCOM.Item oOldItem;
-
-
-        static void SBO_Application_ItemEvent(string FormUID, ref SAPbouiCOM.ItemEvent pVal, out bool BubbleEvent)
-        {
-            //throw new NotImplementedException();
-            BubbleEvent = true;
-            if ((pVal.FormType == 60150 && pVal.EventType != SAPbouiCOM.BoEventTypes.et_FORM_UNLOAD) && pVal.BeforeAction == true) {
-                oForm = Application.SBO_Application.Forms.GetFormByTypeAndCount(pVal.FormType, pVal.FormTypeCount);
-                if (pVal.EventType == SAPbouiCOM.BoEventTypes.et_FORM_LOAD && pVal.BeforeAction == true) {
-                    SAPbouiCOM.Button obt = null;
-                    oOldItem = oForm.Items.Item("2");
-                    oItem = oForm.Items.Add("btnPrint", SAPbouiCOM.BoFormItemTypes.it_BUTTON);
-                    oItem.Top = oOldItem.Top;
-                    oItem.Height = oOldItem.Height;
-                    oItem.Left = oOldItem.Left + oOldItem.Width + 5;
-                    oItem.Width = oOldItem.Width + 20;
-                    obt = (SAPbouiCOM.Button)oItem.Specific;
-                    obt.Caption = "Stampa";
-                }
-                if (pVal.ItemUID == "btnPrint" && (pVal.EventType == SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED || pVal.EventType == SAPbouiCOM.BoEventTypes.et_CLICK) && pVal.BeforeAction == true) {
-
-                    formAttrezzatura.formAttrezzatura form = new formAttrezzatura.formAttrezzatura();
-
-                    form.readAllForm(oForm);
-                    //Application.SBO_Application.MessageBox(valoreCodiceArticolo.Value, 1, "ok", "", "");
-                }
-            }
-        
+            Events ev = null;
+            ev = new Events();
+            System.Windows.Forms.Application.Run();
         }
 
 
 
-        static void SBO_Application_AppEvent(SAPbouiCOM.BoAppEventTypes EventType)
-        {
-            switch (EventType)
-            {
-                case SAPbouiCOM.BoAppEventTypes.aet_ShutDown:
-                    //Exit Add-On
-                    System.Windows.Forms.Application.Exit();
-                    break;
-                case SAPbouiCOM.BoAppEventTypes.aet_CompanyChanged:
-                    break;
-                case SAPbouiCOM.BoAppEventTypes.aet_FontChanged:
-                    break;
-                case SAPbouiCOM.BoAppEventTypes.aet_LanguageChanged:
-                    break;
-                case SAPbouiCOM.BoAppEventTypes.aet_ServerTerminition:
-                    break;
-                default:
-                    break;
-            }
-        }
     }
 }
